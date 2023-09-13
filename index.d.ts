a4f9be96dcc04c65f801c6292d2569d851e81d76
Amir Farzamnia <102469398+amirfarzamnia@users.noreply.github.com>
2023-09-13 18:25:50 +0200

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module 'velocedb' {
+    interface veloceConfig {
+        encoding?: string;
+        space?: number;
+    }
+
+    export default class veloce {
+        constructor(filename: string, config?: veloceConfig);
+        save(): void;
+    }
+  }
