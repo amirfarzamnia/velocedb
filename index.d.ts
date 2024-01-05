f481441d9a8afa50968da41c3015e01fcc17bd37
amirfarzamnia <amir2452fa@gmail.com>
2024-01-05 10:54:01 +0100

    Added comments and optimized codes

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,20 @@
 declare module 'velocedb' {
     export default class Veloce {
+        /**
+         * Constructor for the Veloce class.
+         *
+         * @param {string} filename - The name of the file associated with the database.
+         * @param {object} config - Optional configuration object with encoding and space properties.
+         */
+
         constructor(filename: string, config?: {
             encoding?: string;
             space?: number;
         });
+
+        /**
+         * Save method for the Veloce databases, used to save changes made to the database.
+         */
 
         save(): void;
     }
