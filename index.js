808b74e3ac4031db8e3e45fa9acc86ead598a8d0
amirfarzamnia <amir2452fa@gmail.com>
2024-06-19 13:32:27 +0330

    improvements

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ import fs from 'node:fs';
 
 export default class Veloce {
     constructor(filename, config = {}) {
-        const dir = path.dirname(filename);
-
-        if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-        if (!fs.existsSync(filename)) fs.writeFileSync(filename, '{}');
-
         this.filename = filename;
         this.config = Object.assign({ encoding: 'utf-8', space: 2 }, config);
-        this.data = JSON.parse(fs.readFileSync(filename, this.config.encoding));
+        this.data = fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename, this.config.encoding)) : {};
     }
 
     save() {
+        const dir = path.dirname(this.filename);
+
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+
         fs.writeFileSync(this.filename, JSON.stringify(this.data, null, this.config.space), { encoding: this.config.encoding });
     }
 
