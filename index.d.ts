df734d25bcdbfc0a0c0fc9b71ce51d2b3b671d7f
amirfarzamnia <amir2452fa@gmail.com>
2024-10-21 13:48:24 +0330

    improvements

--- index.d.ts
+++ index.d.ts
@@ -2,18 +2,25 @@
 interface VeloceConfig {
     /** The number of spaces for indentation when saving the file as a local file. Default is 2. */
     space?: number | null;
+
     /** Should the database run in debug mode? In debug mode, it will create logs of all processes and changes. Default is false. */
     debug?: boolean;
+
     /** Should data be automatically saved to the database? This feature only works in proxy mode. Default is true. */
     autoSave?: boolean;
+
     /** Should the database use no proxy mode? The no-proxy mode disables many features and creates a straightforward process for the databases. This mode is more optimized, but you need to save the data manually. Default is false. */
     noProxy?: boolean;
+
     /** The `onUpdate` function is only used in proxy mode. Whenever a new update is received for the data, this function will be triggered with the update method and result. By default, this function is undefined. */
     onUpdate?: (method: string, result: any) => void;
+
     /** The options that will be used for the `node:fs` module when saving the data to the database. */
     fileOptions?: object | { encoding: 'utf-8' };
+
     /** This object is used in proxy mode. In JavaScript, proxies require a handler to work. This object is the handler used for data proxies. Modifying this object is not suggested. */
     handler?: object;
+
     /** This is the data that will be put into the database during the database construction process. If the database exists, the data will be the database's data; otherwise, the data will be an empty object. */
     target?: any;
 }
