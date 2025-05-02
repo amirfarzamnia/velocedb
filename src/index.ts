cb72c1d463f6fdbd5dfb37adea505c3379660d7a
amirfarzamnia <amir2452fa@gmail.com>
2025-05-02 02:30:56 -0600

    added json-stringify-safe, radash, and secure-json-parse dependencies; updated Veloce class to utilize secure-json-parse and json-stringify-safe for improved data handling

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
 import path from "node:path";
 import fs from "node:fs";
+import sjson from "secure-json-parse";
+import stringify from "json-stringify-safe";
+import { isObject } from "radash";
 
-/** Configuration options for the Veloce database. */
-export interface VeloceConfig {
-  /** The number of spaces for indentation when saving the file as a local file. Default is 2. */
-  space?: number | null;
-
-  /** Should data be automatically saved to the database? This feature only works in proxy mode. Default is true. */
-  autoSave?: boolean;
-
-  /** Should the database use no proxy mode? The no-proxy mode disables many features and creates a straightforward process for the databases. This mode is more optimized, but you need to save the data manually. Default is false. */
-  noProxy?: boolean;
-
-  /** When automatically saving the database data, the database will wait for the given duration in milliseconds before saving the data. If the database is modified again, it will wait again until there are no more changes, then save the data. The default value is set to 750 milliseconds (750). */
-  autoSaveTimeoutMs?: number;
-
-  /** The timeout in milliseconds before retrying to save the data if any issues occur. Default is 100 milliseconds (100). */
-  savingRetryTimeout?: number;
-
-  /** The `onUpdate` function is only used in proxy mode. Whenever a new update is received for the data, this function will be triggered with the update method and result. By default, this function is undefined. */
-  onUpdate?: (method: string, result: any) => void;
-
-  /** The database will create timeouts before saving the data (only in auto-save mode). This number indicates maximum timeouts before saving database data. Default is 10. */
-  maximumAutoSaveTimeouts?: number;
-
-  /** The options that will be used for the `node:fs` module when saving the data to the database. */
-  fileOptions?: object | { encoding: string };
-
-  /** This object is used in proxy mode. In JavaScript, proxies require a handler to work. This object is the handler used for data proxies. Modifying this object is not suggested. */
-  handler?: ProxyHandler<any>;
-
-  /** This is the data that will be put into the database during the database construction process. If the database exists, the data will be the database's data; otherwise, the data will be an empty object. */
-  target?: any;
-}
-
-export default class Veloce {
+export default class Veloce<Data = unknown> {
   private filename: string;
 
-  private config: Required<VeloceConfig>;
+  private config: {
+    /** The number of spaces for indentation when saving the file as a local file. Default is 2. */
+    space?: number | null;
 
-  public data: any;
+    /** Should data be automatically saved to the database? This feature only works in proxy mode. Default is true. */
+    autoSave?: boolean;
+
+    /** Should the database use no proxy mode? The no-proxy mode disables many features and creates a straightforward process for the databases. This mode is more optimized, but you need to save the data manually. Default is false. */
+    noProxy?: boolean;
+
+    /** When automatically saving the database data, the database will wait for the given duration in milliseconds before saving the data. If the database is modified again, it will wait again until there are no more changes, then save the data. The default value is set to 750 milliseconds (750). */
+    autoSaveTimeoutMs?: number;
+
+    /** The timeout in milliseconds before retrying to save the data if any issues occur. Default is 100 milliseconds (100). */
+    savingRetryTimeout?: number;
+
+    /** The `onUpdate` function is only used in proxy mode. Whenever a new update is received for the data, this function will be triggered with the update method and result. By default, this function is undefined. */
+    onUpdate?: (method: string, result: any) => void;
+
+    /** The database will create timeouts before saving the data (only in auto-save mode). This number indicates maximum timeouts before saving database data. Default is 10. */
+    maximumAutoSaveTimeouts?: number;
+
+    /** The options that will be used for the `node:fs` module when saving the data to the database. */
+    fileOptions?: fs.WriteFileOptions;
+
+    /** This object is used in proxy mode. In JavaScript, proxies require a handler to work. This object is the handler used for data proxies. Modifying this object is not suggested. */
+    handler: ProxyHandler<any>;
+
+    /** This is the data that will be put into the database during the database construction process. If the database exists, the data will be the database's data; otherwise, the data will be an empty object. */
+    target?: Data;
+  };
+
+  public data: Data;
 
   private initialCheckIsDone?: boolean;
 
@@ -53,7 +53,7 @@ export default class Veloce {
     return new Proxy(target, handler);
   }
 
-  constructor(filename: string, config: Partial<VeloceConfig> = {}) {
+  constructor(filename: string, config: Veloce["config"]) {
     this.filename = filename;
 
     this.config = {
@@ -71,7 +71,7 @@ export default class Veloce {
 
           this.config.onUpdate?.("get", result);
 
-          if (typeof result === "object" && result !== null) {
+          if (isObject(result)) {
             return Veloce.createNestedProxies(result, this.config.handler);
           }
 
@@ -184,7 +184,7 @@ export default class Veloce {
             this.save();
           }
 
-          if (typeof result === "object" && result !== null) {
+          if (isObject(result)) {
             return Veloce.createNestedProxies(result, this.config.handler);
           }
 
@@ -193,7 +193,7 @@ export default class Veloce {
         construct: (
           target: any,
           argumentsList: any[],
-          newTarget: Function
+          newTarget: any
         ): object => {
           const result = Reflect.construct(target, argumentsList, newTarget);
 
@@ -203,7 +203,7 @@ export default class Veloce {
             this.save();
           }
 
-          if (typeof result === "object" && result !== null) {
+          if (isObject(result)) {
             return Veloce.createNestedProxies(result, this.config.handler);
           }
 
@@ -214,7 +214,7 @@ export default class Veloce {
     };
 
     this.config.target = fs.existsSync(filename)
-      ? JSON.parse(fs.readFileSync(filename, { encoding: "utf-8" }))
+      ? sjson(fs.readFileSync(filename, { encoding: "utf-8" }))
       : {};
     Object.assign(this.config, config);
 
@@ -239,7 +239,7 @@ export default class Veloce {
 
       fs.writeFileSync(
         this.filename,
-        JSON.stringify(this.data, null, this.config.space),
+        stringify(this.data, null, this.config.space),
         this.config.fileOptions
       );
 
@@ -269,10 +269,12 @@ export default class Veloce {
     if (this.saveTimeout) {
       clearTimeout(this.saveTimeout);
 
-      this.saveTimeoutsCount = this.saveTimeoutsCount || 0;
+      this.saveTimeoutsCount = this.saveTimeoutsCount ?? 0;
       this.saveTimeoutsCount++;
 
-      if (this.saveTimeoutsCount >= this.config.maximumAutoSaveTimeouts) {
+      if (
+        this.saveTimeoutsCount >= (this.config.maximumAutoSaveTimeouts ?? 0)
+      ) {
         save();
 
         return;
